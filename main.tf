96f4045b41f4e97cf1ede0c90c4bc70b7cb311a2
Jeremiah Norris <jjno91@gmail.com>
2022-07-28 13:57:16 -0500

    rename intermediate local

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 locals {
-  git_log   = file("${path.module}/.git/logs/HEAD")
-  last_line = regex("\n.*\n.*\\z", local.git_log)
-  last_list = split(" ", local.last_line)
-  git_hash  = local.last_list[1]
+  git_log    = file("${path.module}/.git/logs/HEAD")
+  last_line  = regex("\n.*\n.*\\z", local.git_log)
+  last_split = split(" ", local.last_line)
+  git_hash   = local.last_split[1]
 }
 
 output "this" {
