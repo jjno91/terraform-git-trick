1e2cffc7c018cba6d1a5ff9889d401be41705448
Jeremiah Norris <jjno91@gmail.com>
2022-07-28 13:36:16 -0500

    pull git hash from filesystem

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-data "git_repository" "this" {
-  path = path.module
-}
-
-output "root" {
-  value = data.git_repository.this.commit_sha
-}
-
-output "module" {
-  value = module.this.this
-}
-
-module "this" {
-  source = "github.com/jjno91/terraform-git-trick?ref=194a1b53bbf7147718258be5b9a7794b471da6a1"
-}
-
-terraform {
-  required_providers {
-    git = {
-      source  = "innovationnorway/git"
-      version = "0.1.3"
-    }
-  }
+output "this" {
+  value = chomp(file("${path.module}/.git/logs/HEAD"))
 }
