9fc60ecd1344f76acd425d3d4e1bd312adebb3ff
Jeremiah Norris <jjno91@gmail.com>
2022-07-28 14:09:33 -0500

    comment the module ref

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   git_log    = file("${path.module}/.git/logs/HEAD")
-  last_line  = regex("\n.*\n.*\\z", local.git_log)
+  last_line  = regex("^.*\n.*\\z", local.git_log)
   last_split = split(" ", local.last_line)
   git_hash   = local.last_split[1]
 }
@@ -8,11 +8,11 @@ locals {
 output "this" {
   value = local.git_hash
 }
-
+/*
 output "module" {
   value = module.this.this
 }
 
 module "this" {
   source = "github.com/jjno91/terraform-git-trick.git?ref=96f4045b41f4e97cf1ede0c90c4bc70b7cb311a2"
-}
+}*/
