341135a1f2330497f6aec1eff56ec853f992481a
Jeremiah Norris <jjno91@gmail.com>
2022-08-01 09:50:05 -0500

    add fallback logic

--- main.tf
+++ main.tf
@@ -1,19 +1,23 @@
+variable "fallback" { default = "latest" }
+variable "path" { default = null }
+
 locals {
-  git_log_path     = "${path.module}/.git/logs/HEAD"
+  path             = var.path == null ? path.root : var.path
+  git_log_path     = "${local.path}/.git/logs/HEAD"
   git_log_contents = fileexists(local.git_log_path) ? chomp(file(local.git_log_path)) : ""
   last_line        = fileexists(local.git_log_path) ? regex(".*\\z", local.git_log_contents) : ""
-  last_split       = fileexists(local.git_log_path) ? split(" ", local.last_line) : []
-  git_hash         = fileexists(local.git_log_path) ? local.last_split[1] : "latest"
+  last_line_tokens = fileexists(local.git_log_path) ? split(" ", local.last_line) : []
+  hash             = fileexists(local.git_log_path) ? local.last_line_tokens[1] : var.fallback
 }
 
-output "this" {
-  value = local.git_hash
+output "hash" {
+  value = local.hash
 }
-
+/*
 output "module" {
-  value = module.this.this
+  value = module.this.hash
 }
 
 module "this" {
   source = "github.com/jjno91/terraform-git-trick.git?ref=0d4519165159221599c0f885a232b1bd23a95d41"
-}
+}*/
